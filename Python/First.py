54ee0b5a5c745e233108e31d28a199ace8320d2a
Joeson Stanes <joesonstanes1360@gmail.com>
2025-11-05 11:22:51 -0600

    first half

--- Python/First.py
+++ Python/First.py
@@ -1,1 +1,57 @@
 print("Hello plano")
+#this is a comment 
+x="hello"
+print(type(x))
+
+#data types in python
+
+#integer
+my_int=10
+my_float=34.89
+
+#string
+my_string="Hello plano!"
+
+#boolena
+
+my_bool=True
+my_bool2=False
+
+#none tells the user that ht number typed is null
+
+my_none=None  
+
+print(my_none)
+
+
+#using arithmetic operations using variable type
+a=20
+b=323.988
+sum=print(a+b)
+
+#string concatenation
+
+first="hello"
+second="plano"
+sum=print(first+""+second)
+
+#operators
+
+# + - * / % // **
+
+#console inout and output
+#output to the console is easy in pyton bcz there si an input erm called inoput 
+
+print("Here is my console op")
+my_inout = input("Enter your name: ")
+print(f"Hello {my_inout}")
+
+#truthy vs falsy values
+#truthy values are values that evalute to true in a boolean context
+
+#falsy 
+#null
+#0,0.00,0j
+
+#Truthy values - Everything else
+
