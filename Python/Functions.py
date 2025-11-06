4327b6f68b154352cde6b4b958cbbcd95eed4b8c
Joeson Stanes <joesonstanes1360@gmail.com>
2025-11-06 13:03:18 -0600

    new commit

---
+++ Python/Functions.py
@@ -0,0 +1,22 @@
+#Functions in Python
+#great way not to repeat some thing that you have alredy done
+# to create function we use the def keyword
+#writing functions are as easy as it sounds we just need to define the function and then call it using the d
+
+def first_function():
+    return "Hello, World!"
+print(first_function())
+def addition(a,b):
+    return a+b
+print(addition(1,2))
+
+# ive annotated my additin function to make to more self documented i can even have an inter[reted type 
+
+#scope of a function
+
+#Local
+#enclosed
+#global
+#built in 
+
+
