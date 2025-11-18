e774e494eb07f7ad2e0f3c504f4071a74a2b8c51
Joeson Stanes <joesonstanes1360@gmail.com>
2025-11-18 14:36:14 -0600

    sql afternoon

--- SQL/SQL intro.sql
+++ SQL/SQL intro.sql
@@ -71,3 +71,74 @@ SELECT genre_id,COUNT(*) AS track_count FROM track
 GROUP BY genre_id
 HAVING count(*)>50
 ORDER BY track_count DESC;
+
+
+-- JOINS ANS SUBQUERIES
+
+--Lets say i want to return the album title and artist name of every album
+SELECT  a.title as album, ar.name as artist
+FROM album as a 
+INNER JOIN artist as ar ON a.artist_id=ar.artist_id
+
+
+--Left join
+SELECT ar.name,a.title 
+FROM artist ar
+LEFT JOIN album a ON ar.artist_id=a.artist_id
+WHERE ar.name LIKE 'A%';
+
+--OUTER JOIN
+
+--RETURNS ALL ROWS FROM BOTH THE TABLES MATCHES RECORDS WHERE NO MATCHES EXIST.ACCESS
+-- eSSSENTIALLY THE COMPLAETE UNION OF BOTH THE TABLES 
+
+SELECT ar.name,a.title
+FROM artist ar
+FULL OUTER JOIN album a ON ar.artist_id=a.artist_id
+WHERE a.title IS NOT NULL;
+
+--Cross JOINS 
+EXPLAIN
+SELECT g.name as genre,
+mt.name as media_type
+FROM genre g
+CROSS JOIN media_type mt 
+WHERE g.name IN ('Rock','Jazz')
+Group BY g.name,mt.name;
+
+--EQUI JOINS and theta joins 
+--equi joins = any join thath use equality conditions in the ON clause 
+
+SELECT 
+     t1.name as track_name,
+     t1.milliseconds as track_length,
+     t2.avg_album_length
+FROM track t1
+INNER JOIN(
+    SELECT album_id,AVG(milliseconds) 
+    FROM track
+    GROUP BY album_id
+) as t2 ON t1.album_id=t2.album_id AND t1.milliseconds>t2.avg_album_length;
+
+--SELF JOIN -Don't see it on the caricula but it is there just to practice
+-- YOu just do a normal join you just do it 
+
+--joining the employee table to itself
+SELECT emp.first_name as employee,
+       mgr.first_name as manager
+FROM employee emp
+LEFT JOIN employee mgr ON emp.reports_to=mgr.employee_id
+
+
+--Sub queries quesrie within quesires we saw one in the inner join nust you can use inside where in or even inside a selsct 
+
+SELECT name,milliseconds FROM track
+WHERE milliseconds>(SELECT AVG(milliseconds) FROM track)
+ORDER BY milliseconds DESC LIMIT 10;
+
+---- THE SUB QUESRY ALWAYS EVALUATES FRIST
+SELECT name, unit_price,
+(SELECT MAX(unit_price) FROM track) AS highest_price,
+(SELECT MIN(unit_price) FROM track) AS lowest_price
+FROM track
+WHERE unit_price>(SELECT AVG(unit_price) FROM track)
